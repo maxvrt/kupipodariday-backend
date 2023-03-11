ce6dc5a5388b7b76772b9b9bf26f21d430096b47
Maksim Sokolov <vonrut@gmail.com>
2023-03-11 22:08:45 +0300

    правки по 3 ревью

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   // Добавим глобальный пайлайн валидации на следующей строке
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(3000);
